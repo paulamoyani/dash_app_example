87f895959be680aec5491ee4dcf7c17bf1ffed46
pm445 <pm445@cornell.edu>
2019-11-25 15:38:21 +0100

    changes in app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
-
-
 import dash
 from dash.dependencies import Input, Output
 import dash_core_components as dcc
@@ -16,21 +13,16 @@ app.layout = html.Div([
     dcc.Input(id='my-id', value='initial value', type="text"),
     html.Div(id='my-div')
 ])
+
 app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 @app.callback(
     Output(component_id='my-div', component_property='children'),
     [Input(component_id='my-id', component_property='value')]
 )
+
 def update_output_div(input_value):
     return 'You\'ve entered. WOW!!! "{}"'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server()
-
-
-# In[ ]:
-
-
-
-
